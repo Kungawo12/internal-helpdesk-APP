8f4930abaa5eb6436fb05afa980b6ab75cc3fdf4
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2026-05-18 09:11:07 -0500

    fix: white background on logo in dark mode only

--- src/components/ui/KarmaStaffLogo.tsx
+++ src/components/ui/KarmaStaffLogo.tsx
@@ -5,7 +5,7 @@ export default function KarmaStaffLogo({ size = 64, className = "" }: { size?: n
       alt="Karma Staff"
       width={size}
       height={size}
-      className={`${className} dark:invert dark:brightness-125 transition-all`}
+      className={`${className} dark:bg-white dark:rounded-md dark:p-0.5 transition-all`}
       style={{ width: size, height: size, objectFit: "contain" }}
     />
   );
